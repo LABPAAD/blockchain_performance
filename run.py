a9319aa4aec65722d9cd3bb4a67097e49b13fb87
Ericksulino <ericksulino@gmail.com>
2022-04-25 13:06:43 -0300

    modificações no print

--- run.py
+++ run.py
@@ -6,15 +6,21 @@ import timeit
 
 N = 5
 T = 10
+list = []
+list1 = []
 #cmd = 'docker exec cli peer chaincode invoke -o orderer.example.com:7050 --tls true --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C mychannel -n emr_contract --peerAddresses peer0.org1.example.com:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt --peerAddresses peer0.org2.example.com:9051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt -c '{"Args":["issue","Pedro","accessinfo","123"]}'
 
 def funcao():
     for i in range(N):
         inicio = timeit.default_timer()
-        #sreturn_code = subprocess.call("echo Hello World", shell=True)
-        return_code = subprocess.call("sleep 2", shell=True)
+        return_code = subprocess.call("echo Hello World", shell=True)
+        #return_code = subprocess.call("sleep 2", shell=True)
         fim = timeit.default_timer()
-        print(i,return_code,fim - inicio)
+        text = "{} {} {} \n".format(i,return_code,fim - inicio)
+        print(text)
+        list1.append(text)
+        
+
         
 
 
@@ -32,9 +38,17 @@ if __name__ == '__main__':
     log_name = sys.argv[3]
     print("experiment:",N, T, log_name)
 
-
+    arquivo = open(log_name, "a")
     for i in range(T):
-        print("execução: ",i)
+        text = "execução: {} \n".format(i)
+        print(text)
+        list.append(text)
         threading.Thread(target=funcao).start()
         time.sleep(1)
+    for i in list:
+        list.append(i)
+    
+    arquivo.writelines(list)
 
+    print("Fim!!")
+    
