758d1a48268e8f19debd16e4eb3571b3b0c9087f
Ericksulino <ericksulino@gmail.com>
2022-04-25 13:40:14 -0300

    passando resultados para um arquivo

--- run.py
+++ run.py
@@ -4,15 +4,19 @@ import time
 import subprocess
 import timeit
 from hashlib import sha256
+import datetime
 
 
 N = 5
 T = 10
 list = []
-list1 = []
 #cmd = 'docker exec cli peer chaincode invoke -o orderer.example.com:7050 --tls true --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C mychannel -n emr_contract --peerAddresses peer0.org1.example.com:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt --peerAddresses peer0.org2.example.com:9051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt -c '{"Args":["issue","Pedro","accessinfo","123"]}'
 
-def funcao():
+def funcao(j):
+    text1 = "execução: {} \n".format(j)
+    list.append(text1)
+    list.append("id return_code tempo   \n")
+        #print(text)
     for i in range(N):
         id_str = sha256(str(time.time()).encode('utf-8')).hexdigest()
         cmd = "sleep 2"
@@ -20,13 +24,9 @@ def funcao():
         return_code = subprocess.call("echo Hello World", shell=True)
         #return_code = subprocess.call("sleep 2", shell=True)
         fim = timeit.default_timer()
-        text = "{} {} {} \n".format(i,return_code,fim - inicio)
-        print(text)
-        list1.append(text)
-        return_code = subprocess.call(cmd, shell=True)
-        fim = timeit.default_timer()
-        print(id_str, return_code, fim - inicio)
-        
+        text = "{}     {}        {} \n".format(i,return_code,fim - inicio)
+        #print(text)
+        list.append(text)
 
 
 #*******************************************************************************
@@ -41,17 +41,13 @@ if __name__ == '__main__':
     N = int(sys.argv[1])
     T = int(sys.argv[2])
     log_name = sys.argv[3]
-    print("experiment:",N, T, log_name)
+    prt = "experiment: {} {} {} {}\n\n\n".format(N, T, log_name, datetime.datetime.now())
+    print(prt)
+    list.append(prt)
     arquivo = open(log_name, "a")
     for i in range(T):
-        text = "execução: {} \n".format(i)
-        print(text)
-        list.append(text)
-    for i in range(T):
-        threading.Thread(target=funcao).start()
+        threading.Thread(target=funcao(i)).start()
         time.sleep(1)
-    for i in list:
-        list.append(i)
     
     arquivo.writelines(list)
 
