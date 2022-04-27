849da18ef9ebeca26100fea60cd1e7b7dba6ef6c
PedroHercules <pedroherculesdantas@gmail.com>
2022-04-27 11:27:45 -0300

    Removendo I/O desnecessarias

--- run.py
+++ run.py
@@ -18,32 +18,23 @@ def timeout():
     cont = False
     print("Tempo esgotado") """
 
-def funcao(j):
-    text1 = "\nexecução: {} \n".format(j)
-    list.append(text1)
-    list.append("id return_code tempo   \n")
-        #print(text)
-
-    #if (cont == False):
-        #print(txCount)
-    #    break
-    id_str = sha256(str(time.time()).encode('utf-8')).hexdigest()
+def funcao(hash):
+    
+   
     cmd2 = '{"Args":["issue","Pedro","accessinfo","' + id_str + '"]}'
     cmd = "docker exec cli2 peer chaincode invoke -o orderer.example.com:7050 --tls true --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C mychannel -n emrcontract --peerAddresses peer0.org1.example.com:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt --peerAddresses peer0.org2.example.com:9051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt -c '{}'".format(cmd2)
 
-    #cmd = "sleep 2"
     inicio = timeit.default_timer()
     return_code = subprocess.call(cmd,stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
-    #return_code = subprocess.call("sleep 2", shell=True)
+    
     fim = timeit.default_timer()
     temp = fim-inicio
     text = [id_str, return_code, temp]
-    #text = "{}     {}        {} \n".format(i,return_code,fim - inicio)
-    #print(text)
+   
     list.append(text)
     global ntrans
     ntrans +=1
-    #list.append("\n{} Transações!\n".format(i+1))
+    
 
 
 #*******************************************************************************
@@ -52,36 +43,33 @@ def funcao(j):
 if __name__ == '__main__':
 
     if len(sys.argv) != 4:
-        print("run.py <tps> <seconds> <log_name>")
         sys.exit(1)
 
     N = int(sys.argv[1])
     T = int(sys.argv[2])
     log_name = sys.argv[3]
     prt = "experiment: {} {} {} {}\n\n\n".format(N, T, log_name, datetime.datetime.now())
-    print(prt)
     list.append(prt)
     arquivo = open(log_name, "a")
+    
+    listThreads = []
     inicio = timeit.default_timer()
-
-    #threading.Thread(target=timeout).start()
-    #listThreads = []
-
     for i in range(T):
+        id_str = sha256(str(time.time()).encode('utf-8')).hexdigest()
         for t in range(N):
-            thread = threading.Thread(target=funcao, args=(i,))
+            thread = threading.Thread(target=funcao, args=(id_str,))
+            listThreads.append(thread)
             thread.start()
         time.sleep(1)
-        ""
     fim = timeit.default_timer()
+    
+    for j in listThreads:
+        j.join()
+    
     temtot = "Tempo total: {}".format(fim-inicio)
     prtntrans = "Numero total de transferencias: {}".format(ntrans)
     list.append(temtot)
     list.append(prtntrans)
-    #arquivo.writelines(list)
     for i in list:
         arquivo.writelines("{}\n".format(i))
-    print("Fim!!")
-    print(temtot)
-    print(prtntrans)
     
